4720a12a36ccbb1c1f3238e26484bab562a00145
naxac <naxac86@gmail.com>
2019-09-13 09:31:14 +0800

    Всякие мелочи

--- gamedata/spawns/!Компиляция.bat
+++ gamedata/spawns/!Компиляция.bat
@@ -1,9 +1,14 @@
 @echo off
+
 copy /Y acdc.pl spawn_unpack\acdc.pl
 xcopy /Y stkutils "spawn_unpack\stkutils\"
 ren all.spawn all.spawn.old
+
 pushd spawn_unpack\
+
 perl acdc.pl -c all.ltx -o ..\all.spawn.new
+
 del /Q acdc.pl
 rd /S /Q stkutils
+
 @pause
